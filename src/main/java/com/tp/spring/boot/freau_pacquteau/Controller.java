abd57f00e813f496215c01d6c79605617c531990
sebastienpacqueteau@gmail.com <HjLkZCYZsvQ2jJk>
2019-03-05 12:07:04 +0100

    Merge

--- src/main/java/com/tp/spring/boot/freau_pacquteau/Controller.java
+++ src/main/java/com/tp/spring/boot/freau_pacquteau/Controller.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Controller {
-    
+
     private final
     YellowJacketService service;
     
